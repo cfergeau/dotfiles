507469ab762539fbe843b6d77cb33b82f194b42b
Christophe Fergeau <christophe@fergeau.eu>
2025-06-08 17:58:25 +0200

    nvim-ks: Switch to everforest colorscheme

---
+++ nvim-ks/.config/nvim-ks/lua/custom/plugins/colorscheme-everforest.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		"neanias/everforest-nvim",
+		version = false,
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- Optionally configure and load the colorscheme
+			-- directly inside the plugin declaration.
+			vim.g.everforest_enable_italic = true
+			vim.g.everforest_background = "soft"
+			vim.cmd.colorscheme("everforest")
+		end,
+	},
+}
