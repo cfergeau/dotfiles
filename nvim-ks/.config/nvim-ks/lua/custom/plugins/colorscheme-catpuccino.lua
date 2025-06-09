291e1337981bf89077a28aa65fdadfa4393a32c9
Christophe Fergeau <christophe@fergeau.eu>
2025-06-09 22:19:13 +0200

    nvim-ks: colorscheme: Switch to catpuccin-frappe

--- nvim-ks/.config/nvim-ks/lua/custom/plugins/colorscheme-catpuccino.lua
+++ nvim-ks/.config/nvim-ks/lua/custom/plugins/colorscheme-catpuccino.lua
@@ -1,15 +1,14 @@
 return {
 	{
-		"neanias/everforest-nvim",
-		version = false,
+		"catppuccin/nvim",
+		name = "catppuccin",
 		lazy = false,
 		priority = 1000,
 		config = function()
 			-- Optionally configure and load the colorscheme
 			-- directly inside the plugin declaration.
-			vim.g.everforest_enable_italic = true
-			vim.g.everforest_background = "soft"
-			vim.cmd.colorscheme("everforest")
+			--vim.g.catppuccin_flavour = "auto"
+			vim.cmd.colorscheme("catppuccin-frappe")
 		end,
 	},
 }
