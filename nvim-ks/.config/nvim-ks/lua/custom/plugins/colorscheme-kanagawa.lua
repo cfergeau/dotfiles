2d8f0c028ef4080aeda342ef9d416ce13b7cfd35
Christophe Fergeau <christophe@fergeau.eu>
2025-06-10 21:19:38 +0200

    nvim-ks: colorscheme: Try kanagawa

--- nvim-ks/.config/nvim-ks/lua/custom/plugins/colorscheme-kanagawa.lua
+++ nvim-ks/.config/nvim-ks/lua/custom/plugins/colorscheme-kanagawa.lua
@@ -1,14 +1,14 @@
 return {
 	{
-		"catppuccin/nvim",
-		name = "catppuccin",
+		"rebelot/kanagawa.nvim",
+		name = "kanagawa",
 		lazy = false,
 		priority = 1000,
 		config = function()
 			-- Optionally configure and load the colorscheme
 			-- directly inside the plugin declaration.
 			--vim.g.catppuccin_flavour = "auto"
-			vim.cmd.colorscheme("catppuccin-frappe")
+			vim.cmd.colorscheme("kanagawa")
 		end,
 	},
 }
