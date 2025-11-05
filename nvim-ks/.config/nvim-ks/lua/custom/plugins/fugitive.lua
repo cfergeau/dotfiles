0984bf20b6d9d8b80d7a480ab5df0c2d27b38b49
Christophe Fergeau <cfergeau@redhat.com>
2025-11-05 15:51:48 +0100

    Revert "nvim-ks: Enable some bindings for fugitive"
    This reverts commit bda84e251f9968f51c3a3cc5f0a16c14191a7b0e.

--- nvim-ks/.config/nvim-ks/lua/custom/plugins/fugitive.lua
+++ nvim-ks/.config/nvim-ks/lua/custom/plugins/fugitive.lua
@@ -1,18 +1,14 @@
 return {
-  {
-    'tpope/vim-fugitive',
-    config = function()
-      --vim-fugitive
-      vim.keymap.set('n', '<leader>gs', ':Git<CR>', { noremap = true, desc = 'git status' }) --git status
-      vim.keymap.set('n', '<leader>ga', ':Git add ', { noremap = true, desc = 'git add ' })
-      vim.keymap.set('n', '<leader>gA', ':Git add .<CR>', { noremap = true, desc = 'git add .' })
-      vim.keymap.set('n', '<leader>gp', ':Git push --quiet <CR>', { noremap = true, desc = 'git push' })
-      vim.keymap.set('n', '<leader>gc', ':Git commit -qam "', { noremap = true, desc = 'git commit -am' })
-      vim.keymap.set({ 'n', 'v' }, '<leader>gH', ':GBrowse', { noremap = true, desc = 'Open in gitweb' })
-    end,
-  },
-  {
-    'tpope/vim-rhubarb',
-  },
+	{
+		"tpope/vim-fugitive",
+		config = function()
+			--vim-fugitive
+			vim.keymap.set("n", "<leader>gs", ":Git<CR>", { noremap = true, desc = "git status" }) --git status
+			vim.keymap.set("n", "<leader>ga", ":Git add ", { noremap = true, desc = "git add " })
+			vim.keymap.set("n", "<leader>gA", ":Git add .<CR>", { noremap = true, desc = "git add ." })
+			vim.keymap.set("n", "<leader>gp", ":Git push --quiet <CR>", { noremap = true, desc = "git push" })
+			vim.keymap.set("n", "<leader>gc", ':Git commit -qam "', { noremap = true, desc = "git commit -am" })
+		end,
+	},
 }
 -- vim: ts=2 sts=2 sw=2 et
