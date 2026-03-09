a90e4c29f79f4a2155822fa36ce1e7856500fbb6
Christophe Fergeau <cfergeau@redhat.com>
2026-03-09 13:04:53 +0100

    nvim-ks: Switch to nvim 0.12 config
    This syncs to nvim-kickstart commit cfdc17be3
    The biggest change is the switch to pack.nvim for plugin management.

--- nvim-ks/.config/nvim-ks/lua/custom/plugins/colorscheme-kanagawa.lua
+++ nvim-ks/.config/nvim-ks/lua/custom/plugins/colorscheme-kanagawa.lua
@@ -1,14 +1,2 @@
-return {
-	{
-		"rebelot/kanagawa.nvim",
-		name = "kanagawa",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			-- Optionally configure and load the colorscheme
-			-- directly inside the plugin declaration.
-			--vim.g.catppuccin_flavour = "auto"
-			vim.cmd.colorscheme("kanagawa")
-		end,
-	},
-}
+vim.pack.add { 'https://github.com/rebelot/kanagawa.nvim' }
+vim.cmd.colorscheme("kanagawa")
