c8593ed0a5875fb2d39e32d7870eee74f244a206
Christophe Fergeau <cfergeau@redhat.com>
2026-05-19 15:10:36 +0200

    nvim-ks: Automatically enable LSP
    This adds the changes to
    https://github.com/nvim-lua/kickstart.nvim/pull/1945 which apparently
    have some issues but at least this makes LSPs work again for my usecase.

--- nvim-ks/.config/nvim-ks/init.lua
+++ nvim-ks/.config/nvim-ks/init.lua
@@ -738,7 +738,6 @@ do
     gh 'neovim/nvim-lspconfig',
     gh 'mason-org/mason.nvim',
     gh 'mason-org/mason-lspconfig.nvim',
-    gh 'WhoIsSethDaniel/mason-tool-installer.nvim',
   }
 
   -- Automatically install LSPs and related tools to stdpath for Neovim
@@ -756,7 +755,7 @@ do
     -- You can add other tools here that you want Mason to install
   })
 
-  require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+  require('mason-lspconfig').setup { ensure_installed = ensure_installed }
 
   for name, server in pairs(servers) do
     vim.lsp.config(name, server)
